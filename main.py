c0407d941470c53d43246e9692ad01711152eced
Leo Bergqvist <leo@bergqvist.se>
2023-05-23 12:12:38 +0200

    Clarifications

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button, Slider, CheckButtons
+from matplotlib.widgets import Button, Slider, CheckButtons  # type: ignore
 import random
 
 
@@ -10,8 +10,16 @@ class sorting_algorithm_visualizer:
         self.operation_delay = operation_delay
         self.operations = 0
 
-    def display_bar(self, arr: list, current_index: int = None):
-        """Displays the bars on the screen"""
+    def display_bar(self, arr: list, current_index: int = -1) -> None:
+        """Displays the array as a bar chart.
+
+        Args:
+            arr: The array to be displayed.
+            index: The index of the element that was just swapped. Defaults to -1.
+
+        Returns:
+            None
+        """
 
         plt.clf()
         bar_colors = ["blue" if i != current_index else "red" for i in range(len(arr))]
@@ -29,8 +37,15 @@ class algorithms:
     def __init__(self):
         pass
 
-    def bubble_sort(self, arr: list):
-        """Sorts the array using bubble sort"""
+    def bubble_sort(self, arr: list) -> None:
+        """Sorts the array using bubble sort
+
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
 
         for i in range(len(arr)):
             for j in range(0, len(arr) - i - 1):
@@ -40,8 +55,15 @@ class algorithms:
 
         btn_handler.create_btns()
 
-    def selection_sort(self, arr: list):
-        """Sorts the array using selection sort"""
+    def selection_sort(self, arr: list) -> None:
+        """Sorts the array using selection sort
+
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
 
         for i in range(len(arr)):
             min_idx = i
@@ -53,8 +75,15 @@ class algorithms:
 
         btn_handler.create_btns()
 
-    def insertion_sort(self, arr: list):
-        """Sorts the array using insertion sort"""
+    def insertion_sort(self, arr: list) -> None:
+        """Sorts the array using insertion sort
+
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
 
         for i in range(1, len(arr)):
             key = arr[i]
@@ -67,10 +96,27 @@ class algorithms:
 
         btn_handler.create_btns()
 
-    def merge_sort(self, arr: list):
-        """Sorts the array using merge sort"""
+    def merge_sort(self, arr: list) -> None:
+        """Sorts the array using merge sort
 
-        def merge(left, right):
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
+
+        def merge(left: list, right: list) -> list:
+            """Merges two sorted arrays into a single sorted array
+
+            Args:
+                left: The left sorted array
+                right: The right sorted array
+
+            Returns:
+                The merged sorted array
+            """
+
             result = []
             i = j = 0
             while i < len(left) and j < len(right):
@@ -84,9 +130,19 @@ class algorithms:
             result.extend(right[j:])
             return result
 
-        def merge_sort_recursive(arr):
+        def merge_sort_recursive(arr: list) -> list:
+            """Recursively sorts the array using merge sort
+
+            Args:
+                arr: The array to be sorted
+
+            Returns:
+                The sorted array
+            """
+
             if len(arr) <= 1:
                 return arr
+
             mid = len(arr) // 2
             left = merge_sort_recursive(arr[:mid])
             right = merge_sort_recursive(arr[mid:])
@@ -99,10 +155,27 @@ class algorithms:
 
         btn_handler.create_btns()
 
-    def quick_sort(self, arr: list):
-        """Sorts the array using quick sort"""
+    def quick_sort(self, arr: list) -> None:
+        """Sorts the array using quick sort
 
-        def partition(low, high):
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
+
+        def partition(low: int, high: int) -> int:
+            """Partitions the sub-array between low and high using the last element as the pivot
+
+            Args:
+                low: The lower index of the sub-array
+                high: The upper index of the sub-array
+
+            Returns:
+                The index of the pivot element
+            """
+
             pivot = arr[high]
             i = low - 1
             for j in range(low, high):
@@ -114,7 +187,17 @@ class algorithms:
             visualizer.display_bar(arr, i + 1)
             return i + 1
 
-        def quick_sort_recursive(low, high):
+        def quick_sort_recursive(low: int, high: int) -> None:
+            """Recursively sorts the sub-array between low and high using the partition function
+
+            Args:
+                low: The lower index of the sub-array
+                high: The upper index of the sub-array
+
+            Returns:
+                None
+            """
+
             if low < high:
                 pi = partition(low, high)
                 quick_sort_recursive(low, pi - 1)
@@ -123,10 +206,27 @@ class algorithms:
         quick_sort_recursive(0, len(arr) - 1)
         btn_handler.create_btns()
 
-    def heap_sort(self, arr: list):
-        """Sorts the array using heap sort"""
+    def heap_sort(self, arr: list) -> None:
+        """Sorts the array using heap sort
 
-        def heapify(n, i):
+        Args:
+            arr: The array to be sorted.
+
+        Returns:
+            None
+        """
+
+        def heapify(n: int, i: int) -> None:
+            """Heapifies the sub-array rooted at i
+
+            Args:
+                n: The length of the sub-array
+                i: The index of the root node of the sub-array
+
+            Returns:
+                None
+            """
+
             largest = i
             left = 2 * i + 1
             right = 2 * i + 2
@@ -163,24 +263,43 @@ class buttons:
         self.last_arr = 30
         self.selected_algorithms = {}
 
-    def select_algorithm(self, label: str):
-        """Selects the algorithm to be used for sorting"""
+    def select_algorithm(self, label: str) -> None:
+        """Selects the algorithm to be used for sorting.
+
+        Args:
+            label: The label of the selected algorithm.
+
+        Returns:
+            None
+        """
 
         if label == "Bubble Sort":
             self.selected_algorithm = algorithm.bubble_sort
+
         elif label == "Selection Sort":
             self.selected_algorithm = algorithm.selection_sort
+
         elif label == "Insertion Sort":
             self.selected_algorithm = algorithm.insertion_sort
+
         elif label == "Merge Sort":
             self.selected_algorithm = algorithm.merge_sort
+
         elif label == "Quick Sort":
             self.selected_algorithm = algorithm.quick_sort
+
         elif label == "Heap Sort":
             self.selected_algorithm = algorithm.heap_sort
 
-    def new_array(self, event: Button):
-        """Creates a new array and displays it on the screen"""
+    def new_array(self, event: Button) -> None:
+        """Creates a new array and displays it on the screen.
+
+        Args:
+            event: The button click event that triggers the function.
+
+        Returns:
+            None
+        """
 
         global arr
         if self.last_arr == len(arr):
@@ -190,17 +309,36 @@ class buttons:
         self.create_btns()
         self.last_arr = len(arr)
 
-    def update_array_size(self, arr_size: int):
-        """Updates the size of the array"""
+    def update_array_size(self, arr_size: int) -> None:
+        """Updates the size of the array.
+
+        Args:
+            arr_size: The new size of the array.
+
+        Returns:
+            None
+        """
 
         global arr
         arr = [random.randint(1, 100) for _ in range(arr_size)]
 
-    def compare_algorithms(self, event: Button):
-        """Compares the sorting algorithms"""
+    def compare_algorithms(self, event: Button) -> None:
+        """Compares the sorting algorithms.
 
+        This function compares the selected sorting algorithms by measuring the number of operations
+        required to sort an array of random integers. The results are displayed in a bar chart.
+
+        Args:
+            event: The button click event that triggers the function.
+
+        Returns:
+            None
+        """
+
+        # Initialize an empty list to store the results
         results = []
 
+        # Iterate over the selected algorithms and measure the number of operations required to sort the array
         for alg_name, alg in self.selected_algorithms.items():
             arr_copy = arr.copy()
             visualizer.operations = 0
@@ -208,6 +346,7 @@ class buttons:
             alg(arr_copy)
             results.append((alg_name, visualizer.operations))
 
+        # Display the results in a bar chart
         plt.clf()
         plt.bar([result[0] for result in results], [result[1] for result in results])
         plt.xlabel("Algorithms")
@@ -216,13 +355,17 @@ class buttons:
         plt.show()
         self.create_btns()
 
-    def create_btns(self):
-        """Creates the buttons on the screen"""
+    def create_btns(self) -> None:
+        """Creates the buttons for selecting the sorting algorithm.
 
-        ax_checkboxes = plt.axes([0.05, 0.05, 0.2, 0.2])
-        ax_start = plt.axes([0.35, 0.05, 0.25, 0.075])
-        ax_new_array = plt.axes([0.65, 0.05, 0.25, 0.075])
-        ax_array_size = plt.axes([0.375, 0.2, 0.25, 0.03])
+        Returns:
+            None
+        """
+
+        ax_checkboxes = plt.axes(tuple([0.05, 0.05, 0.2, 0.2]))
+        ax_start = plt.axes(tuple([0.35, 0.05, 0.25, 0.075]))
+        ax_new_array = plt.axes(tuple([0.65, 0.05, 0.25, 0.075]))
+        ax_array_size = plt.axes(tuple([0.375, 0.2, 0.25, 0.03]))
 
         self.btn_start = Button(ax_start, "Start")
         self.btn_start.on_clicked(self.compare_algorithms)
@@ -258,9 +401,17 @@ class buttons:
         )
         self.checkboxes.on_clicked(self.toggle_algorithm)
 
-    def toggle_algorithm(self, label):
-        """Toggles the selected algorithms for comparison"""
+    def toggle_algorithm(self, label: str) -> None:
+        """Toggles the selected algorithms for comparison
 
+        Args:
+            label: The label of the algorithm to toggle
+
+        Returns:
+            None
+        """
+
+        # Dictionary mapping algorithm labels to their corresponding functions
         alg = {
             "Bubble Sort": algorithm.bubble_sort,
             "Selection Sort": algorithm.selection_sort,
@@ -270,6 +421,8 @@ class buttons:
             "Heap Sort": algorithm.heap_sort,
         }
 
+        # If the algorithm is already selected, remove it from the selected algorithms
+        # Otherwise, add it to the selected algorithms
         if label in self.selected_algorithms:
             del self.selected_algorithms[label]
         else:
@@ -284,7 +437,7 @@ if __name__ == "__main__":
     arr = [random.randint(1, 100) for _ in range(30)]
 
     fig = plt.figure(figsize=(12, 8))
-    fig.canvas.manager.set_window_title("Sorting Algorithm Visualizer")
+    fig.canvas.manager.set_window_title("Sorting Algorithm Visualizer")  # type: ignore
     plt.subplots_adjust(bottom=0.3)
     ax_bar = fig.add_subplot(111)
 
